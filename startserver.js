84f6fe65fa184c7419ecc0063affcfac093aca14
hydra325 <91817442+hydra325@users.noreply.github.com>
2024-01-28 13:10:49 +0530

    Update startserver.js

--- startserver.js
+++ startserver.js
@@ -1,10 +1,12 @@
-var app=require('./server')
-const mongoose = require('mongoose');
-const startServer = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/merndb');
-    app.listen(4000, () => {
-        console.log('server is ready')
-    })
+import { app } from "./server.js";
+const mongoose = require("mongoose");
+
+
+async function startServer() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/MERNDB");
+  app.listen(4000, () => {
+    console.log("Server is ready...");
+  });
 }
 
-module.exports=startServer;
+module.exports.startServer = startServer;
